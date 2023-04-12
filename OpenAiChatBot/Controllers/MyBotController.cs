a9b3c6c08f9d3872b04a372f0ac333fac965a479
Vivek Patel <vivek.mvp@gmail.com>
2023-04-12 15:38:21 -0500

    Initial commit

---
+++ OpenAiChatBot/Controllers/MyBotController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenAiChatBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyBotController : ControllerBase
+    {
+
+    }
+}
