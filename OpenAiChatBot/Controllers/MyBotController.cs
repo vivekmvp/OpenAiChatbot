56b858ea2c9924d867f06b1d977fb555c4348f11
Vivek Patel <vivek.mvp@gmail.com>
2023-04-12 16:19:53 -0500

    Consume OPEN AI API

--- OpenAiChatBot/Controllers/MyBotController.cs
+++ OpenAiChatBot/Controllers/MyBotController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OpenAI_API.Completions;
+using OpenAI_API;
+using System.Text;
 
 namespace OpenAiChatBot.Controllers
 {
@@ -7,6 +10,31 @@ namespace OpenAiChatBot.Controllers
     [ApiController]
     public class MyBotController : ControllerBase
     {
+        //Only demo purpose - I have use key inline.
+        //Dont put the key in your code.
+        string openAIKey = "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";
 
+        [HttpGet]
+        [Route("search")]
+        public async Task<IActionResult> Search(string query)
+        {   
+            var openai = new OpenAIAPI(openAIKey);
+
+            //Create Request
+            CompletionRequest completionRequest = new CompletionRequest();              
+            completionRequest.Prompt = query;
+            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+
+            //Call API
+            var response = await openai.Completions.CreateCompletionAsync(completionRequest);
+
+            //Build Output
+            var sbOutput = new StringBuilder();            
+            foreach (var completion in response.Completions)
+            {
+                sbOutput.Append(completion.Text);
+            }
+            return Ok(sbOutput.ToString());
+        }
     }
 }
