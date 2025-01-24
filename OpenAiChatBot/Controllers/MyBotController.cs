5b6c7d585f3266ec8915800b175f160dd0c34199
Ndumiso Buthelezi <ndzucain@gmail.com>
2025-01-24 12:20:52 +0200

    phushed gpt.35

--- OpenAiChatBot/Controllers/MyBotController.cs
+++ OpenAiChatBot/Controllers/MyBotController.cs
@@ -1,8 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using OpenAI_API.Completions;
+﻿using Microsoft.AspNetCore.Mvc;
 using OpenAI_API;
-using System.Text;
+using OpenAI_API.Chat;
+using System.Threading.Tasks;
 
 namespace OpenAiChatBot.Controllers
 {
@@ -10,31 +9,30 @@ namespace OpenAiChatBot.Controllers
     [ApiController]
     public class MyBotController : ControllerBase
     {
-        //Only demo purpose - I have use key inline.
-        //Dont put the key in your code.
-        string openAIKey = "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";
+        private const string openAIKey = "your-api-key-here";
 
         [HttpGet]
         [Route("search")]
         public async Task<IActionResult> Search(string query)
-        {   
+        {
             var openai = new OpenAIAPI(openAIKey);
 
-            //Create Request
-            CompletionRequest completionRequest = new CompletionRequest();              
-            completionRequest.Prompt = query;
-            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
-
-            //Call API
-            var response = await openai.Completions.CreateCompletionAsync(completionRequest);
-
-            //Build Output
-            var sbOutput = new StringBuilder();            
-            foreach (var completion in response.Completions)
+            var chatRequest = new ChatRequest()
             {
-                sbOutput.Append(completion.Text);
-            }
-            return Ok(sbOutput.ToString());
+                // Either update your NuGet package and use:
+                // Model = OpenAI_API.Models.Model.ChatGPTTurbo,
+
+                // OR use the direct model name string:
+                Model = "gpt-3.5-turbo",
+
+                Messages = new[]
+                {
+                    new ChatMessage(ChatMessageRole.User, query)
+                }
+            };
+
+            var response = await openai.Chat.CreateChatCompletionAsync(chatRequest);
+            return Ok(response.Choices[0].Message.Content);
         }
     }
 }
